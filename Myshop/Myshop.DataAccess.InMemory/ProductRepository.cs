d976fab8d7a80927efca6f6598817a43451a7835
markdewit <39710865+markdewit@users.noreply.github.com>
2019-05-24 22:15:28 +0200

    Models And storade

---
+++ Myshop/Myshop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+
+namespace Myshop.DataAccess.InMemory
+{
+    class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products;
+
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+           if(products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+        public void Commit() {
+            cache["producct"] = products;
+
+        }
+        public void Insert(Product p) {
+            products.Add(p);
+        }
+        public void Update(Product product)
+        {
+            Product productToUpdate = products.Find(p => p.Id == product.Id);
+
+            if (productToUpdate != null)
+            {
+                productToUpdate = product;
+            }
+            else {
+                throw new Exception("produkt nicht gefunden");
+            }
+        }
+        public Product Find(string Id)
+        {
+            Product product = products.Find(p => p.Id == Id);
+
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("produkt nicht gefunden");
+            }
+        }
+
+        public IQueryable<Product> Collection() {
+            return products.AsQueryable();
+        }
+        public void Delete(string Id)
+        {
+            Product productToDelete = products.Find(p => p.Id == Id);
+
+            if (productToDelete != null)
+            {
+                products.Remove(productToDelete);
+            }
+            else
+            {
+                throw new Exception("produkt nicht gefunden");
+            }
+        }
+    }
+}
